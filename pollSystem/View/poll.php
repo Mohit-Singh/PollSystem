cc045caa6cadd066f6f0c03f945ae024f0982c17
Prateek <prateek.saini@osscube.com>
2013-06-03 00:56:41 -0700

    login

---
+++ pollSystem/View/poll.php
@@ -0,0 +1,49 @@
+        <div class="box-in">
+          <h2>Welcome to Polling System</h2>
+         <div id="divMainPageContainer">
+
+<div id="divMainPageCenter">
+
+<input type="button" name="createNewPoll" value="createNewPoll" onclick="openCreateNewPoll();"/>
+<div id="divCreateNewPoll">
+<form id="submitForm">
+<table id="addMoreOptionsTable">
+<tr>
+<td>
+<label>CREATE POLL</label></td>
+<td>
+<textarea rows="1" cols="50"></textarea>
+</td>
+</tr>
+<tr>
+<td>
+<label>Option</label>
+</td>
+<td>
+<input type="text"/>
+</td>
+</tr>
+<tr>
+<td>
+<label>Option</label>
+</td>
+<td>
+<input type="text"/>
+</td>
+</tr>
+</table>
+<input type="submit" value="Submit"></input>
+
+</form>
+<div id="addMoreOptions">
+
+</div>
+<input type="button" name="addMoreOptions" value="Add More Options" onclick="addMoreOptions();"/>
+
+
+
+</div>
+
+</div>
+</div>
+</div>
