917a402a02c07fd046bb15277f657eacb25e3014
ramandeepsinghoss <ramandeep.singh@osscube.com>
2013-06-03 00:33:39 -0700

    viewQuestion

---
+++ pollSystem/misc/prototypes/html/ViewQuestion.php
@@ -0,0 +1,27 @@
+<?php
+?>
+
+<html>
+<head>
+<title></title>
+</head>
+<body>
+	<div>
+		<table>
+			<tr>
+				<td colspan="4"><textarea name="question" rows="5" cols="30"
+						style="width: 370; height: 70;"></textarea></td>
+			</tr>
+			<tr>
+				<td><input type="radio" name="option" value="1">1<br> <input
+					type="radio" name="option" value="2">2<br> <input type="radio"
+					name="option" value="3">3<br> <input type="radio" name="option"
+					value="4">4<br></td>
+			</tr>
+			<tr>
+				<td colspan="4"><input type="button" onclick="" value="Submit" /></td>
+			</tr>
+		</table>
+	</div>
+</body>
+</html>
