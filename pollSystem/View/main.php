ac1e3da5fa763cb198b57e18e6e866aa5809268c
kawaljeetsingh <kawaljeet.singh@osscube.com>
2013-06-04 08:41:11 -0700

    main updated

--- pollSystem/View/main.php
+++ pollSystem/View/main.php
@@ -4,9 +4,9 @@
 <title>Polling System</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="View/css/style.css" type="text/css"
-	media="screen, projection, tv" />
+media="screen, projection, tv" />
 <link rel="stylesheet" href="View/css/style-print.css" type="text/css"
-	media="print" />
+media="print" />
 
 <style>
 </style>
@@ -18,107 +18,103 @@
 
 <script src="View/jquery1/jquery.mousewheel-3.0.4.pack.js"></script>
 <link rel="stylesheet" href="View/jquery1/jquery.fancybox-1.3.4.css"
-	type="text/css" media="screen">
+type="text/css" media="screen"/>
 
 
-	<script>
+<script>
 $(document).ready(function(){
 
-	<?php 
-			if(isset($_SESSION["username"]))
-			{
-				
-				echo "$.post('index.php',{'loginStatus':'true'},function(data){ $('#column-left').hide(); $('#column-right').html(data); $('#logOut').show();  });";
-			}
-			else 
-			{
-				echo  "$('#logOut').hide();";
-			}
-			
+<?php
+if(isset($_SESSION["username"]))
+{
 
-	?>
+echo "$.post('index.php',{'loginStatus':'true'},function(data){ $('#column-left').hide(); $('#column-right').html(data); $('#logOut').show(); });";
+}
+else
+{
+echo "$('#logOut').hide();";
+}
 
 
-	loadAllPoll();
-	
-	$("#divCreateNewPoll").hide();
-	
-	$("#login").fancybox({
-        // 'width'            : screenW/2,
-        // 'height'        : screenH-210,
-        // 'autoScale'        : false,
-        // 'transitionIn'        : 'none',
-        // 'transitionOut'        : 'none',
-        // 'type'            : 'iframe'
-     });
+?>
 
-	$("#register").fancybox({
-        // 'width'            : screenW/2,
-        // 'height'        : screenH-210,
-        // 'autoScale'        : false,
-        // 'transitionIn'        : 'none',
-        // 'transitionOut'        : 'none',
-        // 'type'            : 'iframe'
-     });
-    $("#signIn").click(function(){
-        $.ajax({
-                url : './index.php?controller=mainController&method=login',
-                type : 'post',
-                data : "userName="+$("#userName").val()+"&password="+$("#password").val(),                
-                success : function(data){
-                   if(data.trim() != "Account Does not exist" && data.trim() != "Password does not match")
-                   {
-	                	$("#column-left").hide(); 
-	                	$('#logOut').show();
-	                    $("#column-right").prepend(data);
 
-                   }
-                   else
-                   {
-                       alert(data.trim());
-                   }
-                }
-        });
-        $.fancybox.close();
-    });    
+loadAllPoll();
+$("#divCreateNewPoll").hide();
+$("#login").fancybox({
+// 'width' : screenW/2,
+// 'height' : screenH-210,
+// 'autoScale' : false,
+// 'transitionIn' : 'none',
+// 'transitionOut' : 'none',
+// 'type' : 'iframe'
+});
+
+$("#register").fancybox({
+// 'width' : screenW/2,
+// 'height' : screenH-210,
+// 'autoScale' : false,
+// 'transitionIn' : 'none',
+// 'transitionOut' : 'none',
+// 'type' : 'iframe'
+});
+$("#signIn").click(function(){
+$.ajax({
+url : './index.php?controller=mainController&method=login',
+type : 'post',
+data : "userName="+$("#userName").val()+"&password="+$("#password").val(),
+success : function(data){
+if(data.trim() != "Account Does not exist" && data.trim() != "Password does not match")
+{
+$("#column-left").hide();
+$('#logOut').show();
+$("#column-right").prepend(data);
+
+}
+else
+{
+alert(data.trim());
+}
+}
+});
+$.fancybox.close();
+});
 });
 
 function loadAllPoll()
 {
-	$.post('index.php',{"controller":"mainController","method":"loadAllPoll"},function(data){
+$.post('index.php',{"controller":"mainController","method":"loadAllPoll"},function(data){
 
-		//alert(data);
-		var result=jQuery.parseJSON(data);
-		var str="<table id='pollTable'>";
-		str+="<tr id='tableHead'><td>Question</td>"+
-			"<td>User Email Id</td>"+
-			"<td>User Name</td>"+
-			"<td>Comment Count</td>"+
-			"<td>Votes Count</td><td>Vote Now</td></tr>";
-		var count=0;
-		$.each(result, function(key,val){
-				if(count%2==0)
-				{
-					str +="<tr id='odd'>";
-				}
-				else
-				{
-					str+="<tr id='even'>";
-				}
-				str +="<td>"+val['question']+"</td>"+
-				"<td>"+val['username']+"</td>"+
-				"<td>"+val['first_name']+" "+val['last_name']+"</td>"+
-				"<td>"+val['comment']+"</td>"+
-				"<td>"+val['votes']+"</td>"+
-				"<td><input type='button' value='Vote Now' onclick='voteNow(\""+val['id']+"\")'>"+
-				"</tr>";
-				count++;
-								
-			});
-		str+="</table>";
-		$("#column-right").append(str);
-		
-	});
+//alert(data);
+var result=jQuery.parseJSON(data);
+var str="<table id='pollTable'>";
+str+="<tr id='tableHead'><td>Question</td>"+
+"<td>User Email Id</td>"+
+"<td>User Name</td>"+
+"<td>Comment Count</td>"+
+"<td>Votes Count</td><td>Vote Now</td></tr>";
+var count=0;
+$.each(result, function(key,val){
+if(count%2==0)
+{
+str +="<tr id='odd'>";
+}
+else
+{
+str+="<tr id='even'>";
+}
+str +="<td>"+val['question']+"</td>"+
+"<td>"+val['username']+"</td>"+
+"<td>"+val['first_name']+" "+val['last_name']+"</td>"+
+"<td>"+val['comment']+"</td>"+
+"<td>"+val['votes']+"</td>"+
+"<td><input type='button' value='Vote Now' onclick='voteNow(\""+val['id']+"\")'>"+
+"</tr>";
+count++;
+});
+str+="</table>";
+$("#column-right").append(str);
+});
 }
 
 function openCreateNewPoll()
@@ -129,94 +125,85 @@ $("#divCreateNewPoll").toggle();
 var rowIndex=2;
 function addMoreOptions()
 {
-	rowIndex++;
-    var row = "<tr id='row_"+rowIndex+"'><td><label>Option</label></td><td><input name='opt"+rowIndex+"' type=text /></td><td><button onclick='removeRow("+rowIndex+");'>X</button></td></tr>";	
-    
-	$("#addMoreOptionsTable").append(row);
+rowIndex++;
+var row = "<tr id='row_"+rowIndex+"'><td><label>Option</label></td><td><input name='opt"+rowIndex+"' type=text /></td><td><button onclick='removeRow("+rowIndex+");'>X</button></td></tr>";
+$("#addMoreOptionsTable").append(row);
 
-	
-	
 }
 function removeRow(rowIndex){
-	
-    $('#row_'+rowIndex).remove();
+$('#row_'+rowIndex).remove();
 }
 
 
 function register() {
-	$.post('index.php',
-			{
-		'controller':'mainController',
-		'method':'registerUser',
-		'firstName':$('#firstName').val(),
-		'lastName':$('#lastName').val(),
-		'email':$('#email').val(),
-		'password':$('#registerPassword').val(),
-			},function(data,status){
-					if(status == "success") {
-						if(data.trim() == "true") {
-							alert("you are successfully registered");
-							parent.$.fancybox.close();
-						}
-						else {
-							alert("same email id alredy registered");
-							parent.$.fancybox.close();
-						}
-					}
-				});
+$.post('index.php',
+{
+'controller':'mainController',
+'method':'registerUser',
+'firstName':$('#firstName').val(),
+'lastName':$('#lastName').val(),
+'email':$('#email').val(),
+'password':$('#registerPassword').val(),
+},function(data,status){
+if(status == "success") {
+if(data.trim() == "true") {
+alert("you are successfully registered");
+parent.$.fancybox.close();
+}
+else {
+alert("same email id alredy registered");
+parent.$.fancybox.close();
+}
+}
+});
 }
 
 function AddQuestion() {
-	$.ajax({
-        url : './index.php?controller=mainController&method=AddQuestion',
-        type : 'post',
-        data : $("#submitForm").serialize(),                
-        success : function(data){
-            alert("Succesfully saved");
-            location.reload();
-            //$("#column-right").load("./View/poll.php");
-            //loadAllPoll();
-        }
+$.ajax({
+url : './index.php?controller=mainController&method=AddQuestion',
+type : 'post',
+data : $("#submitForm").serialize(),
+success : function(data){
+alert("Succesfully saved");
+location.reload();
+//$("#column-right").load("./View/poll.php");
+//loadAllPoll();
+}
 });
 }
 
 function logOutUser() {
-	$.ajax({
-        url : './index.php?controller=mainController&method=userLogOut',
-        type : 'post',          
-        success : function(data){
-            location.reload();
-        }
+$.ajax({
+url : './index.php?controller=mainController&method=userLogOut',
+type : 'post',
+success : function(data){
+location.reload();
+}
 });
 }
 
 function viewPreviousPolls()
 {
-	$.ajax({
-        url : './index.php?controller=mainController&method=loadPreviousPoll',
-        type : 'post',
-        async:false,
-//         data : "userName="+$("#userName").val()+"&password="+$("#password").val(),                
-        success : function(data){
-     
-         
-            $("#box1").html('');
-            $("#box1").append(data);
-        }
-		});
+$.ajax({
+url : './index.php?controller=mainController&method=loadPreviousPoll',
+type : 'post',
+async:false,
+// data : "userName="+$("#userName").val()+"&password="+$("#password").val(),
+success : function(data){
+$("#box1").html('');
+$("#box1").append(data);
+}
+});
 
-	
-	 $.ajax({
-         url : './index.php?controller=mainController&method=viewPreviousPolls',
-         type : 'post',
-//          data : "userName="+$("#userName").val()+"&password="+$("#password").val(),                
-         success : function(data){
-           
-          
-             $("#previousPollDiv").html('');
-             $("#previousPollDiv").append(data);
-         }
- 	});
+$.ajax({
+url : './index.php?controller=mainController&method=viewPreviousPolls',
+type : 'post',
+// data : "userName="+$("#userName").val()+"&password="+$("#password").val(),
+success : function(data){
+$("#previousPollDiv").html('');
+$("#previousPollDiv").append(data);
+}
+});
 }
 
 
@@ -225,107 +212,97 @@ function viewPreviousPolls()
 <style>
 #logOut
 {
-	float: right;
-	margin-right: 50px;
+float: right;
+margin-right: 50px;
 }
 #pollTable
 {
-	margin-left: 25px;
-	text-align: center;
-	color: #0A0A2A;
-	font-family: 'Tangerine', serif;
-  font-size: 12px;
+margin-left: 25px;
+text-align: center;
+color: #0A0A2A;
+font-family: 'Tangerine', serif;
+font-size: 12px;
 }
 #pollTable #odd
 {
-	background-color : #A9F5BC;
-	color:#0A0A2A;
-	
+background-color : #A9F5BC;
+color:#0A0A2A;
 }
 #pollTable #even
 {
-	background-color : #F5F6CE;
-	color:#0A0A2A;
-	
+background-color : #F5F6CE;
+color:#0A0A2A;
 }
 #pollTable #tableHead
 {
-	background-color : #2EFEF7;
-	color:#0A0A2A;
-	
+background-color : #2EFEF7;
+color:#0A0A2A;
 }
 
 </style>
 
 </head>
 <body>
-	<div id="logOut"><a href="#" onclick="logOutUser();">LogOut</a></div>
-	<div id="wrapper">
-	
-	<hr class="noscreen" />
-  <div class="content">
-    <div id = "column-left" class="column-left">
-      <h3>LOGIN/REGISTER</h3>
-         
-       <p><a id="register" href="#registerDiv">REGISTER</a></p>
-       <p><a id="login" href="#loginDiv">LOGIN</a></p>
-       
-     <p><a id="viewPolling" href="#" onclick="viewPreviousPolls();">VIEW POLLINGS</a></p> 
+<div id="header">gfgfgfg</div>
+<div id="logOut"><a href="#" onclick="logOutUser();">LogOut</a></div>
+<div id="wrapper">
+<div class="content">
+<div id = "column-left" class="column-left">
+<h3>LOGIN/REGISTER</h3>
+<p><a id="register" href="#registerDiv">REGISTER</a></p>
+<p><a id="login" href="#loginDiv">LOGIN</a></p>
+<p><a id="viewPolling" href="#" onclick="viewPreviousPolls();">VIEW POLLINGS</a></p>
 
-		<div class="hidden">
-			<div id="loginDiv">
-				<h2>LOGIN</h2>
-				<label>USERNAME:</label> <input type="text" id="userName"
-					name="userName" /></br> <label>PASSWORD:</label> <input type="password"
-					id="password" name="password" /></br> <input type="button"
-					id="signIn" name="signIn" value="LOGIN" />
-			</div>
-		</div>
+<div class="hidden">
+<div id="loginDiv">
+<h2>LOGIN</h2>
+<label>USERNAME:</label> <input type="text" id="userName"
+name="userName" /></br> <label>PASSWORD:</label> <input type="password"
+id="password" name="password" /></br> <input type="button"
+id="signIn" name="signIn" value="LOGIN" />
+</div>
+</div>
 
-		<div class="hidden">
-			<div id="registerDiv">
-				<h2>REGISTER</h2>
-				<table>
-					<tr>
-						<td><label>FIRSTNAME:</label></td>
-						<td><input type="text" name="firstName" id="firstName" /></br></td>
-					</tr>
-					<tr>
-						<td><label>LASTNAME:</label></td>
-						<td><input type="text" name="lastName" id="lastName" /></br></td>
-					</tr>
-					<tr>
-						<td><label>PASSWORD:</label></td>
-						<td><input type="password" name="password" id="registerPassword" /></br></td>
-					</tr>
-					<tr>
-						<td><label>EMAIL:</label></td>
-						<td><input type="text" name="email" id="email" /></br></td>
-					</tr>
-					<tr>
-						<td><input type="button" name="register" value="REGISTER"
-							onclick="register()" /></td>
-					</tr>
-				</table>
-			</div>
-		</div>
+<div class="hidden">
+<div id="registerDiv">
+<h2>REGISTER</h2>
+<table>
+<tr>
+<td><label>FIRSTNAME:</label></td>
+<td><input type="text" name="firstName" id="firstName" /></br></td>
+</tr>
+<tr>
+<td><label>LASTNAME:</label></td>
+<td><input type="text" name="lastName" id="lastName" /></br></td>
+</tr>
+<tr>
+<td><label>PASSWORD:</label></td>
+<td><input type="password" name="password" id="registerPassword" /></br></td>
+</tr>
+<tr>
+<td><label>EMAIL:</label></td>
+<td><input type="text" name="email" id="email" /></br></td>
+</tr>
+<tr>
+<td><input type="button" name="register" value="REGISTER"
+onclick="register()" /></td>
+</tr>
+</table>
+</div>
+</div>
 
 
-	</div>
+</div>
 
-	<div id="column-right" class="column-right">
-	
-		<div id="box1" class="box"></div>
-	</div>
+<div id="column-right" class="column-right">
+<div id="box1" class="box"></div>
+</div>
 
-	</div>
-	<div class="cleaner">&nbsp;</div>
-	</div>
-	</div>
-	<div align=center>
-		OSSCUBE<a href='http://www.osscube.com'> OSSCUBE.COM</a>
-	</div>
+</div>
+</div>
+</div>
+<div align=center>
+OSSCUBE<a href='http://www.osscube.com'> OSSCUBE.COM</a>
+</div>
 </body>
 </html>
-
-
